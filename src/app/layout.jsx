28c78ea6bca9baa2266295b2fe4ff5628f17e7fb
CROSS9111 <hirotoshikurosu@gmail.com>
2024-11-23 21:32:15 +0900

    second commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,18 +7,38 @@ import Logo from '../components/Logo'; // Logoをインポート
 import { usePathname } from 'next/navigation';
 
 export default function RootLayout({ children }) {
-  // const pathname = usePathname();
+  // // const pathname = usePathname();
 
-  // // サイドバーを非表示にするページのパスを配列で定義
-  // const noSidebarPaths = ['/login_user', '/login_manager', '/'];
+  // // // サイドバーを非表示にするページのパスを配列で定義
+  // // const noSidebarPaths = ['/login_user', '/login_manager', '/'];
 
-  // // 現在のパスがnoSidebarPathsのいずれかに一致するか判定
-  // const isNoSidebarPage = noSidebarPaths.includes(pathname);
+  // // // 現在のパスがnoSidebarPathsのいずれかに一致するか判定
+  // // const isNoSidebarPage = noSidebarPaths.includes(pathname);
+
+  // return (
+  //   <html lang="ja">
+  //     <body>{children}</body>
+      
+  //   </html>
+  const pathname = usePathname();
+
+  // サイドバーを非表示にするページのパスを配列で定義
+  const noSidebarPaths = ['/login_user', '/login_manager', '/'];
+
+  // 現在のパスがnoSidebarPathsのいずれかに一致するか判定
+  const isNoSidebarPage = noSidebarPaths.includes(pathname);
 
   return (
     <html lang="ja">
-      <body>{children}</body>
-      
+      <body>
+        <div className="flex">
+          {!isNoSidebarPage && <Sidebar />} {/* 特定ページ以外でサイドバーを表示 */}
+          <div className={isNoSidebarPage ? 'flex-grow p-4' : 'ml-60 flex-grow p-4'}>
+            {!isNoSidebarPage && <Logo />} {/* 特定ページ以外でロゴを表示 */}
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
